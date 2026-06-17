b307b7d23692fdfb985674fee34f5588e4378108
s-uei <s-uei@users.noreply.github.com>
2026-06-17 09:19:24 +0900

    修正(install.sh): OpenCommitの設定をファイル書き込みからコマンドに変更

--- install.sh
+++ install.sh
@@ -56,25 +56,6 @@ if [ ! -d ~/.oh-my-zsh ]; then
 fi
 
 # OpenCommit
-cat >"$HOME/.opencommit" << 'EOF'
-OCO_ONE_LINE_COMMIT=true
-OCO_MODEL=gpt-4o-mini
-OCO_API_URL=undefined
-OCO_PROXY=undefined
-OCO_API_CUSTOM_HEADERS=undefined
-OCO_AI_PROVIDER=openai
-OCO_TOKENS_MAX_INPUT=4096
-OCO_TOKENS_MAX_OUTPUT=500
-OCO_DESCRIPTION=false
-OCO_EMOJI=false
-OCO_LANGUAGE=en
-OCO_MESSAGE_TEMPLATE_PLACEHOLDER=$msg
-OCO_PROMPT_MODULE=conventional-commit
-OCO_TEST_MOCK_TYPE=commit-message
-OCO_OMIT_SCOPE=false
-OCO_GITPUSH=true
-OCO_WHY=false
-OCO_HOOK_AUTO_UNCOMMENT=false
-EOF
-echo "OCO_API_KEY=${OPENAI_API_KEY}" >>$HOME/.opencommit
+$BREWBIN/oco config set OCO_API_KEY=$OPENAI_API_KEY
+$BREWBIN/oco config set OCO_MODEL=gpt-4o-mini
 
